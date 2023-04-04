b7064b628b63da603ebcce0ba766a945600fdc36
Andres66666 <cya2024773@est.univalle.edu>
2023-04-04 12:27:01 -0400

    esto es un uevo cambio de los estilos

--- js/llamar.js
+++ js/llamar.js
@@ -5,11 +5,11 @@ var operacion;
 
 function init(){  // inicializa todas las funciones de lo botones varaibles  
 //variables
+
 var resultado = document.getElementById('resultado');
-var reset = document.getElementById('reset');
 var historial = document.getElementById('historial');
 
-var igual = document.getElementById('igual');
+
 var uno = document.getElementById('uno');
 var dos = document.getElementById('dos');
 var tres = document.getElementById('tres');
@@ -19,148 +19,162 @@ var seis = document.getElementById('seis');
 var siete = document.getElementById('siete');
 var ocho = document.getElementById('ocho');
 var nueve = document.getElementById('nueve');
+var asteristico = document.getElementById('asteristico');
 var cero = document.getElementById('cero');
+var numeral = document.getElementById('numeral');
+
+var mensaje = document.getElementById('mensaje');
+var llamar = document.getElementById('llamar'); 
+var ver = document.getElementById('ver');
+
+
+var atras = document.getElementById('atras');
+var salir = document.getElementById('salir')
+var eliminar= document.getElementById('eliminar');
+
 
 //Eventos de click
 uno.onclick = function(e){
-resultado.textContent = resultado.textContent + "1";
-historial.textContent += "1";
+    click= new Audio("../audios/click.mp3");
+    click.play();
+    resultado.textContent = resultado.textContent + "1";
+    historial.textContent += "1";
 }
 dos.onclick = function(e){
-resultado.textContent = resultado.textContent + "2";
-historial.textContent += "2";
+    click= new Audio("../audios/click.mp3");
+    click.play();
+    resultado.textContent = resultado.textContent + "2";
+    historial.textContent += "2";
 }
 tres.onclick = function(e){
-resultado.textContent = resultado.textContent + "3";
-historial.textContent += "3";
+    click= new Audio("../audios/click.mp3");
+    click.play();
+    resultado.textContent = resultado.textContent + "3";
+    historial.textContent += "3";
 }
 cuatro.onclick = function(e){
-resultado.textContent = resultado.textContent + "4";
-historial.textContent += "4";
+    click= new Audio("../audios/click.mp3");
+    click.play();
+    resultado.textContent = resultado.textContent + "4";
+    historial.textContent += "4";
 }
 cinco.onclick = function(e){
-resultado.textContent = resultado.textContent + "5";
-historial.textContent += "5";
+    click= new Audio("../audios/click.mp3");
+    click.play();
+    resultado.textContent = resultado.textContent + "5";
+    historial.textContent += "5";
 }
 seis.onclick = function(e){
-resultado.textContent = resultado.textContent + "6";
-historial.textContent += "6";
+    click= new Audio("../audios/click.mp3");
+    click.play();
+    resultado.textContent = resultado.textContent + "6";
+    historial.textContent += "6";
 }
 siete.onclick = function(e){
-resultado.textContent = resultado.textContent + "7";
-historial.textContent += "7";
+    click= new Audio("../audios/click.mp3");
+    click.play();
+    resultado.textContent = resultado.textContent + "7";
+    historial.textContent += "7";
 }
 ocho.onclick = function(e){
-resultado.textContent = resultado.textContent + "8";
-historial.textContent += "8";
+    click= new Audio("../audios/click.mp3");
+    click.play();
+    resultado.textContent = resultado.textContent + "8";
+    historial.textContent += "8";
 }
 nueve.onclick = function(e){
-resultado.textContent = resultado.textContent + "9";
-historial.textContent += "9";
+    click= new Audio("../audios/click.mp3");
+    click.play();
+    resultado.textContent = resultado.textContent + "9";
+    historial.textContent += "9";
+}
+asteristico.onclick = function(e){
+    click= new Audio("../audios/click.mp3");
+    click.play();
+    resultado.textContent = resultado.textContent + "*";
+    historial.textContent += "*";
 }
 cero.onclick = function(e){
-resultado.textContent = resultado.textContent + "0";
-historial.textContent += "0";
+    click= new Audio("../audios/click.mp3");
+    click.play();
+    resultado.textContent = resultado.textContent + "0";
+    historial.textContent += "0";
 }
-reset.onclick = function(e){
-resetear();
-historial.textContent ="";
-}
-suma.onclick = function(e){
-operandoa = resultado.textContent;
-if(historial.textContent.length <5)
-{
-historial.textContent +=" + ";
-}
-else
-{
-    //* solo permite de 3 digitos*//
-alert("Solo puede introducir dos valores y 3 digitos como máximo para los dos números, vuelva a intentarlo");
-resetear();
-historial.textContent = "";
-}
-operacion = "+";
-limpiar();
-
-}
-resta.onclick = function(e){
-operandoa = resultado.textContent;
-if(historial.textContent.length <5)
-{
-historial.textContent +=" - ";
-}
-else
-{
-alert("Solo puede introducir dos valores y 3 digitos como máximo para los dos números, vuelva a intentarlo");
-resetear();
-historial.textContent = "";
-}
-operacion = "-";
-limpiar();
-}
-multiplicacion.onclick = function(e){
-operandoa = resultado.textContent;
-if(historial.textContent.length <5)
-{
-historial.textContent +=" x ";
-}
-else
-{
-alert("Solo puede introducir dos valores y 3 digitos como máximo para los dos números, vuelva a intentarlo");
-resetear();
-historial.textContent = "";
-}
-operacion = "*";
-limpiar();
-}
-division.onclick = function(e){
-operandoa = resultado.textContent;
-if(historial.textContent.length <5)
-{
-historial.textContent +=" / ";
-}
-else
-{
-alert("Solo puede introducir dos valores y 3 digitos como máximo para los dos números, vuelva a intentarlo");
-resetear();
-historial.textContent = "";
-}
-operacion = "/";
-limpiar();
-}
-igual.onclick = function(e){
-operandob = resultado.textContent;
-resolver();
-}
+numeral.onclick = function(e){
+    click= new Audio("../audios/click.mp3");
+    click.play();
+    resultado.textContent = resultado.textContent + "#";
+    historial.textContent += "#";
 }
 
-function limpiar(){
-resultado.textContent = "";
-}
-function resetear(){
-resultado.textContent = "";
-operandoa = 0;
-operandob = 0;
-operacion = "";
+
+mensaje.onclick = function(e){
+    click= new Audio("../audios/click.mp3");
+    click.play();
 }
 
-function resolver(){
-var res = 0;
-switch(operacion){
-case "+":
-res = parseFloat(operandoa) + parseFloat(operandob);
-break;
-case "-":
-res = parseFloat(operandoa) - parseFloat(operandob);
-break;
-case "*":
-res = parseFloat(operandoa) * parseFloat(operandob);
-break;
-case "/":
-res = parseFloat(operandoa) / parseFloat(operandob);
-break;
+
+llamar.onclick = function(e){
+    llamando();
 }
-resetear();
-resultado.textContent = res;
-historial.textContent += " = "+res;
+
+function llamando(){
+    const llamarBtn = document.getElementById("llamar");
+    const click = new Audio("../audios/llamando.mp3");
+    click.play();
+    llamarBtn.style.backgroundColor = "red";
+    
+    click.addEventListener('ended', function() {
+        llamarBtn.style.backgroundColor = "";
+    });
+    
+    llamarBtn.addEventListener('dblclick', function() {
+        colgar(click, llamarBtn);
+    });
+}
+
+function colgar(audio, button) {
+    audio.pause();
+    audio.currentTime = 0;
+    button.style.backgroundColor = "";
+}
+
+
+
+
+
+
+ver.onclick = function(e){
+    click= new Audio("../audios/click.mp3");
+    click.play();
+}
+atras.onclick = function(e){
+    click= new Audio("../audios/click.mp3");
+    click.play();
+}
+salir.onclick = function(e){
+    click= new Audio("../audios/click.mp3");
+    click.play();
+}
+eliminar.onclick = function(e){
+    click= new Audio("../audios/click.mp3");
+    click.play();
+    eliminarNumero();
+    eliminarNumeroHistorial();
+}
+
+
+
+
+function eliminarNumero() {
+    let resultado = document.getElementById("resultado").innerHTML;
+    let nuevoResultado = resultado.slice(0, -1);
+    document.getElementById("resultado").innerHTML = nuevoResultado;
+}
+
+function eliminarNumeroHistorial() {
+    let historial = document.getElementById("historial").innerHTML;
+    let nuevoResultado = historial.slice(0, -1);
+    document.getElementById("historial").innerHTML = nuevoResultado;
+}
 }
